fa84df308ab6d0c8e69347dead11bc630cf94a9f
Nick Payne <ndpayne@gmail.com>
2022-10-04 10:48:05 +0100

    initial commit

---
+++ SwiftyWebby/SwiftyWebby/SwiftyWebbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyWebbyApp.swift
+//  SwiftyWebby
+//
+//  Created by Nick Payne on 04/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyWebbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
